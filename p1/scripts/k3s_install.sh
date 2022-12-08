58abd7a2eb768ed0ff2b39bcbf2be9f2245a1a9d
Dario Pinto <laradiopotin@gmail.com>
2022-12-08 16:29:58 +0100

    add flannel iface eth1

--- p1/scripts/k3s_install.sh
+++ p1/scripts/k3s_install.sh
@@ -8,14 +8,14 @@ if [ ${K3S_MODE} == "AGENT" ]; then
         K3S_URL="https://${VM_SERVER_IP}:6443"              \
         K3S_NODE_NAME="${VM_WORKER_HOSTNAME}"               \
         K3S_NODE_IP="${VM_WORKER_IP}"                       \
-        INSTALL_K3S_EXEC="agent --node-ip ${VM_WORKER_IP}"  \
+        INSTALL_K3S_EXEC="agent --node-ip ${VM_WORKER_IP} --flannel-iface eth1"  \
         sh -
 else
     # k3s installation as server
     curl -sfL https://get.k3s.io |                          \
         K3S_KUBECONFIG_MODE="644"                           \
         K3S_NODE_NAME="${VM_SERVER_HOSTNAME}"               \
-        INSTALL_K3S_EXEC="server --node-ip ${VM_SERVER_IP}" \
+        INSTALL_K3S_EXEC="server --node-ip ${VM_SERVER_IP} --flannel-iface eth1" \
         sh -
 
     # save kubectl node token
