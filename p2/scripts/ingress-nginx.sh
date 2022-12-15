d024624c9f7803de511da91ff1248635fd72ea31
Dario Pinto <laradiopotin@gmail.com>
2022-12-15 17:14:46 +0100

    add some content for app.yaml and ingress install script

--- p2/scripts/ingress-nginx.sh
+++ p2/scripts/ingress-nginx.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 curl https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.5.1/deploy/static/provider/cloud/deploy.yaml -O
+
 k apply -f deploy.yaml
+
+k wait --namespace ingress-nginx --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=120s
