7cc12f747c5631e1c2f30d328d70de4cd7952e6b
Thomas BAILLY-SALINS <tbailly-@student.42.fr>
2022-12-19 18:50:45 +0100

    add auto-restart capability to node-app, and it is now listening on port 80

--- p2/node-app/app/index.js
+++ p2/node-app/app/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 
 const app = express()
-const port = 8000
+const port = 80
 
 app.get('/', (req, res) => {
     res.sendFile('/home.html', {
@@ -9,6 +9,10 @@ app.get('/', (req, res) => {
     })
 })
 
+app.get('/500', (req, res) => {
+  process.exit(1)
+})
+
 app.listen(port, () => {
-  console.log(`Listening on port :${port}`)
+  console.log(`node-app listening on port :${port}`)
 })
