26e994ff1e2f632387702e77da62bd272b78a9a1
Dario Pinto <laradiopotin@gmail.com>
2022-12-21 13:31:46 +0100

    add documentation in main README and p2 config files

--- p2/scripts/install_all.sh
+++ p2/scripts/install_all.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
+# Update system, install bare minimum requirements
 echo "\t\nyum UPDATE...\n"
 sudo uname -a
 sudo yum check-update
 sudo yum -y install net-tools
 sudo yum -y update
 
+# install docker for the pulling of the docker images mentionned in the appX.yaml scripts
 curl https://releases.rancher.com/install-docker/19.03.sh | sh
 
 # k3s installation as server
 echo "Installing K3s"
 
+# Options :
+# --bind-address      : k3s bind address
+# --node-ip           : IP address of the node.
+# --tls-san           : Add additional hostnames or IPv4/IPv6 addresses as Subject Alternative Names on the TLS cert
+# --advertise-address : IPv4 address that apiserver uses to advertise to members of the cluster
+# --docker            : Use cri-dockerd instead of containerd
 curl -sfL https://get.k3s.io |                          \
     K3S_KUBECONFIG_MODE="644"                           \
     K3S_NODE_NAME="${VM_SERVER_HOSTNAME}"               \
