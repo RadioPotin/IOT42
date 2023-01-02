27b14247b132fd1b54442a7397705a664bf03d43
Dario Pinto <laradiopotin@gmail.com>
2023-01-02 13:45:22 +0100

    add working cluster installation, onto further setup

--- p3/scripts/.bashrc
+++
@@ -1,1 +0,0 @@
-export PATH=/home/vagrant/.local/bin:/home/vagrant/bin:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/usr/local/bin:/usr/local/bin
--- p3/scripts/install_all.sh
+++ p3/scripts/install_all.sh
@@ -3,11 +3,15 @@
 var=""
 install() {
     var=$1
-    echo "[p3/scripts/install_all.sh] Installing $var ..."
+    echo "***********************************************"
+    echo "[p3/scripts/install_all.sh] INSTALLING $var ..."
+    echo "***********************************************"
 }
 
 ok() {
+    echo "***********************************************"
     echo "[p3/scripts/install_all.sh] $var OK !"
+    echo "***********************************************"
 }
 
 echo "CHANGING TO VAULT REPO DUE TO EOL"
@@ -25,17 +29,17 @@ export PATH=$PATH:/usr/local/bin
 echo "export PATH=$PATH:/usr/local/bin" >> .bashrc
 source .bashrc
 
-install "Utilities and optionnals"
+install "UTILITIES AND OPTIONNALS"
 
 sudo yum install -y net-tools yum-utils
 sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
 sudo yum install -y neovim python3-neovim
 
-ok "Utilities and optionnals"
+ok
 
 # INFO ON https://docs.docker.com/engine/install/centos/
 
-install "Docker on centos"
+install "DOCKER ON CENTOS"
 
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 sudo yum install -y docker-ce docker-ce-cli containerd.io
@@ -49,20 +53,13 @@ sudo systemctl start docker
 # add user to docker group
 
 sudo usermod -aG docker ${USER}
+sudo usermod -aG docker vagrant
 
-ok "Docker on centos"
-
-# INFO ON https://k3d.io/v5.4.6/
-
-install k3d
-
-curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
-
-ok k3d
+ok
 
 # INFO ON https://www.tecmint.com/install-a-kubernetes-cluster-on-centos-8/
 
-install kubectl
+install KUBECTL
 
 sudo cat <<EOF > /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
@@ -75,7 +72,25 @@ gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cl
 EOF
 
 sudo yum install -y kubectl
+#sudo chown -R $USER:$USER /home/$USER/.kube/
+## RUNNING AS USER
+#export KUBECONFIG=$HOME/.kube/config
+#echo 'export KUBECONFIG=$HOME/.kube/config' >> $HOME/.bashrc
+#sudo chown -R $USER:$USER /home/$USER/.kube/
+## RUNNING AS ROOT
+#export KUBECONFIG=/home/vagrant/.kube/config
+#echo 'export KUBECONFIG=/home/vagrant/.kube/config' >> /home/vagrant/.bashrc
+#sudo chown -R vagrant:vagrant /home/vagrant/.kube/
 
-ok kubectl
+ok
+
+# INFO ON https://k3d.io/v5.4.6/
+
+install K3D
+
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+
+ok
+
 
 echo "/!\\ DONE /!\\"
