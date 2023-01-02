666bce207c1bc65f1ffe96a851db06aa104c6eec
Dario Pinto <laradiopotin@gmail.com>
2023-01-02 13:45:22 +0100

    add working cluster installation, onto further setup

--- p3/scripts/setup_all.sh
+++ p3/scripts/setup_all.sh
@@ -4,25 +4,46 @@ echo "SETUP SCRIPT"
 
 
 var=""
-setup() {
+setup()
+{
     var=$1
-    echo "[p3/scripts/install_all.sh] Installing $var ..."
+    echo "*********************************************"
+    echo "[p3/scripts/setup_all.sh] SETTING UP $var ..."
+    echo "*********************************************"
 }
 
-ok() {
-    echo "[p3/scripts/install_all.sh] $var OK !"
+ok()
+{
+    echo "*********************************************"
+    echo "[p3/scripts/setup_all.sh] $var OK !"
+    echo "*********************************************"
 }
 
-setup k3d
+export PATH=$PATH:/usr/local/bin
+echo "export PATH=$PATH:/usr/local/bin" >> .bashrc
+source .bashrc
+
+setup K3D
 
 # INFO ON https://k3d.io/v5.4.6/usage/exposing_services/
 
-k3d cluster create p3 --api-port 8080:80@loadbalancer --api-port 8888:8888@loadbalancer --api-port 8443:443@loadbalancer
 
-setup kubectl
+sudo /usr/local/bin/k3d cluster create p3   \
+  --api-port 6443 --servers 1 --agents 2    \
+  --port 8080:80@loadbalancer               \
+  --port 8443:443@loadbalancer
 
-# INFO ON https://serverfault.com/questions/981012/kubernetes-wait-on-pod-job/1013636
-kubectl get jobs -n kube-system
-kubectl -n kube-system wait --for=condition=complete --timeout=-1s jobs/helm-install-traefik-crd
-kubectl -n kube-system wait --for=condition=complete --timeout=-1s jobs/helm-install-traefik
-kubectl get jobs -n kube-system
+ok
+
+sudo mv /root/.kube /home/vagrant/.kube
+
+sudo chown -R $USER:$USER /home/$USER/.kube/
+
+#setup KUBECTL
+
+## INFO ON https://serverfault.com/questions/981012/kubernetes-wait-on-pod-job/1013636
+#kubectl -n kube-system wait --for=condition=complete --timeout=-1s jobs/helm-install-traefik-crd
+#kubectl -n kube-system wait --for=condition=complete --timeout=-1s jobs/helm-install-traefik
+#kubectl get jobs -n kube-system
+
+#ok
