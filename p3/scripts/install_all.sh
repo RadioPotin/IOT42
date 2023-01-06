3af0dabf1905b4b01fba810f742dab20bb3d84f1
Dario Pinto <laradiopotin@gmail.com>
2023-01-06 13:46:22 +0100

    remove Vagrantfile, add p3 scripts and partial setup

--- p3/scripts/install_all.sh
+++ p3/scripts/install_all.sh
@@ -1,96 +1,51 @@
 #!/bin/bash
-
 var=""
-install() {
+install()
+{
     var=$1
     echo "***********************************************"
     echo "[p3/scripts/install_all.sh] INSTALLING $var ..."
     echo "***********************************************"
 }
 
-ok() {
+ok()
+{
     echo "***********************************************"
-    echo "[p3/scripts/install_all.sh] $var OK !"
+    echo "[p3/scripts/install_all.sh] $var INSTALLED !"
     echo "***********************************************"
 }
 
-echo "CHANGING TO VAULT REPO DUE TO EOL"
-cd /etc/yum.repos.d/
-sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
-sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+install_done()
+{
+    echo "***********************************************"
+    echo "********* INSTALLATION OVER ********"
+    echo "***********************************************"
+}
 
-echo "UPDATING SYSTEM"
-sudo uname -a
-sudo yum check-update
-sudo yum -y install net-tools
-sudo yum -y update
+sudo usermod -aG root ${USER}
+sudo usermod -aG root vagrant
 
-export PATH=$PATH:/usr/local/bin
-echo "export PATH=$PATH:/usr/local/bin" >> .bashrc
-source .bashrc
+# WILL FILL THIS SCRIPT WITH NECESSARY COMMANDS ONCE GUEST OS IS SELECTED
 
-install "UTILITIES AND OPTIONNALS"
-
-sudo yum install -y net-tools yum-utils
-sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
-sudo yum install -y neovim python3-neovim
-
+install "EOL VAULT REPOS"
 ok
 
-# INFO ON https://docs.docker.com/engine/install/centos/
+install "SYSTEM UPDATE"
+ok
 
-install "DOCKER ON CENTOS"
+install "UTILITIES AND OPTIONNALS"
+ok
 
-sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install -y docker-ce docker-ce-cli containerd.io
-
-# https://jolthgs.wordpress.com/2020/05/07/first-step-with-docker-in-arch-linux/
-
-sudo systemctl enable --now docker.service
-sudo systemctl enable --now containerd.service
-sudo systemctl start docker
-
-# add user to docker group
-
-sudo usermod -aG docker ${USER}
-sudo usermod -aG docker vagrant
-
+install "DOCKER"
 ok
 
 # INFO ON https://www.tecmint.com/install-a-kubernetes-cluster-on-centos-8/
-
-install KUBECTL
-
-sudo cat <<EOF > /etc/yum.repos.d/kubernetes.repo
-[kubernetes]
-name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
-enabled=1
-gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-EOF
-
-sudo yum install -y kubectl
-#sudo chown -R $USER:$USER /home/$USER/.kube/
-## RUNNING AS USER
-#export KUBECONFIG=$HOME/.kube/config
-#echo 'export KUBECONFIG=$HOME/.kube/config' >> $HOME/.bashrc
-#sudo chown -R $USER:$USER /home/$USER/.kube/
-## RUNNING AS ROOT
-#export KUBECONFIG=/home/vagrant/.kube/config
-#echo 'export KUBECONFIG=/home/vagrant/.kube/config' >> /home/vagrant/.bashrc
-#sudo chown -R vagrant:vagrant /home/vagrant/.kube/
-
+install "KUBECTL"
 ok
 
 # INFO ON https://k3d.io/v5.4.6/
-
 install K3D
-
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
-
 ok
 
-
-echo "/!\\ DONE /!\\"
+install_done
---
+++ p3/scripts/p3.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+var=""
+setup()
+{
+    var=$1
+    echo "*********************************************"
+    echo "[p3/scripts/setup_all.sh] SETTING UP $var ..."
+    echo "*********************************************"
+}
+
+ok()
+{
+    echo "*********************************************"
+    echo "[p3/scripts/setup_all.sh] $var SET UP !"
+    echo "*********************************************"
+}
+
+setup_start()
+{
+    echo "*********************************************"
+    echo "******* DOCKER,KUBECTL,K3D SETUP START ******"
+    echo "*********************************************"
+}
+
+setup_done()
+{
+    echo "*********************************************"
+    echo "******* DOCKER,KUBECTL,K3D SETUP DONE *******"
+    echo "*********************************************"
+}
+
+setup_start
+
+setup "K3D"
+k3d cluster create p3 \
+  --api-port 6443 \
+  --servers 1 \
+  --agents 2 \
+  --port 8080:80@loadbalancer \
+  --port 8443:443@loadbalancer \
+  --wait \
+  --verbose
+ok
+
+setup "KUBECTL"
+kubectl -n kube-system wait --for=condition=complete --timeout=-1s jobs/helm-install-traefik-crd
+kubectl -n kube-system wait --for=condition=complete --timeout=-1s jobs/helm-install-traefik
+kubectl get jobs -n kube-system
+ok
+
+setup "NAMESPACES"
+kubectl create namespace argocd
+kubectl create namespace dev
+ok
+
+setup "ARGOCD"
+kubectl apply -f Programmation/System/IOT42/p3/config/install_argocd.yaml -n argocd
+echo "*********************************************************"
+echo "***************** Waiting for pods... *******************"
+echo "*********************************************************"
+kubectl wait --for=condition=Ready --timeout=-1s pods --all -n argocd
+kubectl apply -f Programmation/System/IOT42/p3/config/argocd_ingress.yaml -n argocd
+kubectl rollout status deployment argocd-server -n argocd
+kubectl rollout status deployment argocd-redis -n argocd
+kubectl rollout status deployment argocd-repo-server -n argocd
+kubectl rollout status deployment argocd-dex-server -n argocd
+# kubectl get pods -A -w
+ok
+
+setup "ARGOCD-CLI"
+curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
+rm argocd-linux-amd64
+ok
+
+echo "********************************************************************"
+echo "Default Password for ARGOCD:"
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d && echo
+echo "********************************************************************"
+
+setup_done
