4fad95418da808a091e65a2a4e76639ef622f854
Dario Pinto <laradiopotin@gmail.com>
2023-03-01 16:49:21 +0100

    finish p3 automatic deployment

--- p3/scripts/install_all.sh
+++ p3/scripts/install_all.sh
@@ -4,23 +4,33 @@ install()
 {
     var=$1
     echo "***********************************************"
-    echo "[p3/scripts/install_all.sh] INSTALLING $var ..."
+    echo "[p3/scripts/install_all.sh] INSTALLING $var ! *"
     echo "***********************************************"
 }
 
 ok()
 {
     echo "***********************************************"
-    echo "[p3/scripts/install_all.sh] $var INSTALLED !"
+    echo "[p3/scripts/install_all.sh] $var INSTALLED ! **"
     echo "***********************************************"
 }
 
 install_done()
 {
     echo "***********************************************"
-    echo "********* INSTALLATION OVER ********"
+    echo "************** INSTALLATION OVER **************"
     echo "***********************************************"
 }
+
+# Install latest release of Docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh ./get-docker.sh
+
+# https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/
+KUBERNETES_RELEASE=$(curl -sL https://dl.k8s.io/release/stable.txt)
+curl -LO "https://dl.k8s.io/release/${KUBERNETES_RELEASE}/bin/linux/amd64/kubectl"
+
+install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
 # INFO ON https://k3d.io/v5.4.6/
 install K3D
