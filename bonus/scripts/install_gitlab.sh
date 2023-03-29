d92e37ea8da5185b0efbe949633b2406ad7a03ec
RadioPotin <laradiopotin@gmail.com>
2023-03-29 17:32:56 +0200

    some more modificatiosn to make bonus work but my computer dies everytime i try it

--- bonus/scripts/install_gitlab.sh
+++ bonus/scripts/install_gitlab.sh
@@ -1,17 +1,31 @@
 
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
 kubectl create namespace gitlab
 
 helm repo add gitlab https://charts.gitlab.io/
 helm repo update
 
-helm upgrade --install gitlab gitlab/gitlab \
-  --timeout 600s \
+#helm upgrade --install gitlab gitlab/gitlab \
+#  --timeout 600s \
+#  --set global.edition=ce \
+#  --set gitlab-runner.install=false \
+#  --set global.hosts.domain=example.com \
+#  --set global.hosts.externalIP=10.0.0.10 \
+#  --set certmanager-issuer.email=me@example.com \
+#  -n gitlab
+
+
+helm install gitlab gitlab/gitlab \
+  --namespace gitlab \
   --set global.edition=ce \
   --set gitlab-runner.install=false \
+  --set certmanager-issuer.email=dariopinto@hotmail.fr \
   --set global.hosts.domain=example.com \
-  --set global.hosts.externalIP=10.0.0.10 \
-  --set certmanager-issuer.email=me@example.com \
-  -n gitlab
+  --set global.nginx-ingress.enableTLS=true \
+  --set global.hosts.externalIP=127.0.0.1
 
 # watch kubectl get all -n gitlab
 # helm status gitlab -n gitlab
@@ -24,9 +38,18 @@ echo "*********************************************************"
 echo "WAITING FOR PODS... *************************************"
 
 kubectl wait --for=condition=available deployments --all -n gitlab
+# kubectl get pods -n gitlab -w
+
+#kubectl -n gitlab expose deployment gitlab --type=LoadBalancer --name=gitlab
+kubectl -n gitlab get services
 
 echo "TYPE THE FOLLOWING TO ACCESS THE GITLAB SERVICE: ********"
 echo "kubectl port-forward svc/gitlab-webservice-default -n gitlab 8082:8080"
+echo "PASSWORD:"
 
+kubectl get secret gitlab-gitlab-initial-root-password -n gitlab -ojsonpath='{.data.password}' | base64 --decode ; echo
+
+# helm delete gitlab -n gitlab
+# kubectl delete namespace gitlab
 echo "*********************************************************"
 echo "*********************************************************"
